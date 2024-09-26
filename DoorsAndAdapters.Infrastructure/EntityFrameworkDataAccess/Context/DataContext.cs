a4fccb3f6093f2fa42a9adad79ddb0615dbd4e8a
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-25 23:12:19 -0300

    Prepare Infra

---
+++ DoorsAndAdapters.Infrastructure/EntityFrameworkDataAccess/Context/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoorsAndAdapters.Infrastructure.EntityFrameworkDataAccess.Context;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        //
+        // https://stackoverflow.com/questions/76865106/net-entity-framework-dbcontext-use-same-model-class-for-multiple-tables
+        //
+    }
+}

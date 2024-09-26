aee2dfced2bd549d61037ed42b1eef6d3600b7c0
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-25 23:12:53 -0300

    Doors

---
+++ DoorsAndAdapters.Application/UseCases/IGlucometerUseCase.cs
@@ -0,0 +1,11 @@
+﻿using DoorsAndAdapters.Domain.Entities;
+
+namespace DoorsAndAdapters.Application.UseCases;
+
+public interface IGlucometerUseCase
+{
+    void AddTest(GlucoseTest test);
+    void UpdateTest(GlucoseTest test);
+    void DeleteTest(GlucoseTest test);
+    List<GlucoseTest> GetAllTest();
+}
---
+++ DoorsAndAdapters.Application/UseCases/IMedicationPlanUseCase.cs
@@ -0,0 +1,11 @@
+﻿using DoorsAndAdapters.Domain.Entities;
+
+namespace DoorsAndAdapters.Application.UseCases;
+
+public interface IMedicationPlanUseCase
+{
+    void AddMedication(Medication Medication);
+    void UpdateMedication(Medication Medication);
+    void DeleteMedication(Medication Medication);
+    List<Medication> GetAllMedication();
+}

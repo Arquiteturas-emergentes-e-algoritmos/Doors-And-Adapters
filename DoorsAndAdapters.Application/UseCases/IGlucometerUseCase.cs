540af3e68f69a29bb3ab6110958249b86b423656
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-26 07:05:18 -0300

    More fixes

--- DoorsAndAdapters.Application/UseCases/IGlucometerUseCase.cs
+++ DoorsAndAdapters.Application/UseCases/IGlucometerUseCase.cs
@@ -6,6 +6,6 @@ public interface IGlucometerUseCase
 {
     void AddTest(GlucoseTest test);
     void UpdateTest(GlucoseTest test);
-    void DeleteTest(GlucoseTest test);
+    void DeleteTests(GlucoseTest test);
     List<GlucoseTest> GetAllTest();
 }

1f8bd147e242b003687d6aaf50aeeb3532ec64ad
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-26 03:21:51 -0300

    Began adapters

--- DoorsAndAdapters.Api/Program.cs
+++ DoorsAndAdapters.Api/Program.cs
@@ -1,3 +1,8 @@
+using DoorsAndAdapters.Application.Repositories;
+using DoorsAndAdapters.Application.Service;
+using DoorsAndAdapters.Application.UseCases;
+using DoorsAndAdapters.Infrastructure.EntityFrameworkDataAccess.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IGlucometerUseCase, GlucometerService>();
+builder.Services.AddTransient<IMedicationPlanUseCase, MedicationPlanService>();
 
 var app = builder.Build();
 

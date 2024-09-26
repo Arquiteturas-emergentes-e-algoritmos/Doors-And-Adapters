715243acb3adda360d65cfd36a0920b8fa38b920
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-25 23:13:07 -0300

    Beyond the doors

---
+++ DoorsAndAdapters.Application/Service/GlucometerService.cs
@@ -0,0 +1,27 @@
+﻿using DoorsAndAdapters.Application.UseCases;
+using DoorsAndAdapters.Domain.Entities;
+
+namespace DoorsAndAdapters.Application.Service;
+
+public class GlucometerService : IGlucometerUseCase
+{
+    public void AddTest(GlucoseTest test)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteTest(GlucoseTest test)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<GlucoseTest> GetAllTest()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTest(GlucoseTest test)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DoorsAndAdapters.Application/Service/MedicationPlanService.cs
@@ -0,0 +1,27 @@
+﻿using DoorsAndAdapters.Application.UseCases;
+using DoorsAndAdapters.Domain.Entities;
+
+namespace DoorsAndAdapters.Application.Service;
+
+public class MedicationPlanService : IMedicationPlanUseCase
+{
+    public void AddMedication(Medication Medication)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteMedication(Medication Medication)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Medication> GetAllMedication()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateMedication(Medication Medication)
+    {
+        throw new NotImplementedException();
+    }
+}

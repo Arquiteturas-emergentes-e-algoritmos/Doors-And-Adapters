ade97216f109def983e8ee235d4dd00dad5b91b1
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-27 17:37:51 -0300

    Domain add

--- DoorsAndAdapters.Domain/ValueObjects/GlucoseTest.cs
+++ DoorsAndAdapters.Domain/ValueObjects/GlucoseTest.cs
@@ -2,9 +2,16 @@
 
 public class GlucoseTest : ValueObject
 {
+    public GlucoseTest() { }
     public GlucoseTest(ushort value)
     {
         Value = value;
+    }
+
+    public GlucoseTest(ushort value, DateTime time)
+    {
+        Value = value;
+        Time = time;
     }
 
     public ushort Value { get; set; } = 100;
